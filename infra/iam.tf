d0fa663d356552056082d97a6d21ba194af2c1ae
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2024-12-04 17:05:26 -0600

    feat: update IAM policies and workflow for lambda

--- infra/iam.tf
+++ infra/iam.tf
@@ -44,3 +44,47 @@ resource "aws_iam_role_policy_attachment" "cngrs_mailer_ses_policy_attachment" {
   role       = aws_iam_role.cngrs_mailer_role.name
   policy_arn = aws_iam_policy.cngrs_mailer_policy.arn
 }
+
+resource "aws_iam_policy" "gh_actions_runners_policy" {
+  name        = "cngrs_mailer_gh_actions_runers_policy"
+  path        = "/"
+  description = "GHA CNGRS Mailer Runners Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:CompleteLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:ListImages",
+          "ecr:BatchDeleteImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage"
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_ecr_repository.cngrs-mailer.arn}"
+      },
+      {
+        Action   = "ecr:GetAuthorizationToken"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = "lambda:UpdateFunctionCode"
+        Effect   = "Allow"
+        Resource = "${aws_lambda_function.cngrs_mailer.arn}"
+      }
+    ]
+  })
+}
+
+data "aws_iam_user" "gh_actions_runners" {
+  user_name = "gha_runners"
+}
+
+resource "aws_iam_user_policy_attachment" "gh_runners_attachment" {
+  user       = data.aws_iam_user.gh_actions_runners.user_name
+  policy_arn = aws_iam_policy.gh_actions_runners_policy.arn
+}
