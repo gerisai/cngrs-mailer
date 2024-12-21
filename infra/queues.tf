115d692e627a8c4fc15446a1c1f38cfe6dddc28d
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2024-12-20 22:45:08 -0600

    fix: queue visibility timeout

--- infra/queues.tf
+++ infra/queues.tf
@@ -1,7 +1,8 @@
 resource "aws_sqs_queue" "cngrs_mail_queue" {
-  name                      = "${local.app_name}-queue.fifo"
-  max_message_size          = 2048
-  message_retention_seconds = 3600
-  receive_wait_time_seconds = 10
-  fifo_queue                = true
+  name                       = "${local.app_name}-queue.fifo"
+  max_message_size           = 2048
+  visibility_timeout_seconds = 120
+  message_retention_seconds  = 3600
+  receive_wait_time_seconds  = 10
+  fifo_queue                 = true
 }
